a73c1d3940b68257c09e2f8fd922141049b98074
dmitkov28 <mitkov.dimitar@yahoo.com>
2026-05-07 18:04:19 +0300

    feat: add ars technica fetch function

---
+++ backend/src/ars_technica.py
@@ -0,0 +1,28 @@
+import calendar
+import time
+from typing import Any
+
+import feedparser
+
+from src.model import Source
+
+
+def get_ars_technica(
+    parse_fn=feedparser.parse,
+    url: str = "https://feeds.arstechnica.com/arstechnica/index",
+) -> list[Source]:
+    response: feedparser.FeedParserDict = parse_fn(url)
+    entries: list[dict[str, Any]] = response.get("entries", [])  # type: ignore
+    return [
+        Source(
+            title=str(entry["title"]),
+            date=(
+                int(calendar.timegm(entry["published_parsed"]))
+                if entry.get("published_parsed")
+                else int(time.time())
+            ),
+            url=str(entry["link"]),
+            tags=[str(tag["term"]) for tag in entry.get("tags", [])],
+        )
+        for entry in entries
+    ]
