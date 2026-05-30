55f43a85e92f6eb5d29f14177786efa81de9cf5b
dmitkov28 <mitkov.dimitar@yahoo.com>
2026-05-30 17:00:18 +0300

    feat: add lobsters

---
+++ backend/src/lobsters.py
@@ -0,0 +1,29 @@
+import calendar
+import time
+
+import feedparser
+
+from src.model import Source
+
+
+def get_lobsters(url: str = "https://lobste.rs/top/1d/rss", parser_fn=feedparser.parse):
+    response = parser_fn(url)
+    breakpoint()
+    entries = response.get("entries")
+    return [
+        Source(
+            title=str(entry["title"]),
+            date=(
+                int(calendar.timegm(entry["pubDate"]))
+                if entry.get("pubDate")
+                else int(time.time())
+            ),
+            url=str(entry["guid"]),
+            tags=[tag.get("term") for tag in entry.get("tags", [])],
+        )
+        for entry in entries
+    ]
+
+
+if __name__ == "__main__":
+    print(get_lobsters())
