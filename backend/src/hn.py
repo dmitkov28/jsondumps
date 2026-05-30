6aacd6264aae472b0d8f091772e522ad825481ff
dmitkov28 <mitkov.dimitar@yahoo.com>
2026-05-30 17:00:11 +0300

    feat: add hn

---
+++ backend/src/hn.py
@@ -0,0 +1,24 @@
+import calendar
+import time
+
+import feedparser
+
+from src.model import Source
+
+
+def get_hn(url: str = "https://hnrss.org/frontpage", parser_fn=feedparser.parse):
+    response = parser_fn(url)
+    entries = response.get("entries")
+    return [
+        Source(
+            title=str(entry["title"]),
+            date=(
+                int(calendar.timegm(entry["pubDate"]))
+                if entry.get("pubDate")
+                else int(time.time())
+            ),
+            url=str(entry["guid"]),
+            tags=[],
+        )
+        for entry in entries
+    ]
